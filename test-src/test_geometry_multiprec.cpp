f3095f8b77bdf7f81fb04ba6dfbd012cfbf0a4c9
Lesleis Nagy <lesleisnagy@gmail.com>
2023-01-28 15:03:39 +0000

    Added test for multi-precision tetrahedron centre.

--- test-src/test_geometry_multiprec.cpp
+++ test-src/test_geometry_multiprec.cpp
@@ -258,3 +258,46 @@ TEST_CASE("Test triangle_area() function for 'multiprecision' type.", "Vector3D 
     REQUIRE( fabs(expected - actual) < eps );
 
 }
+
+TEST_CASE("Test tetrahedron_center() function for 'multiprecision' type.", "Vector3D geometry") {
+
+    using namespace org::lesleisnagy::geomlib;
+
+    using std::string;
+    using mpfr::mpreal;
+
+    using Vec3D = Vector3D<mpreal>;
+    const int digits = 50;
+    mpreal::set_default_prec(mpfr::digits2bits(digits));
+
+    Vec3D::set_eps(1E-20);
+    Vec3D r1(1.0, 0.0, 0.0);
+    Vec3D r2(0.0, 1.0, 0.0);
+    Vec3D r3(0.0, 0.0, 1.0);
+    Vec3D r4(1.0, 1.0, 1.0);
+
+    mpreal eps = 1E-40;
+    Vec3D expected(mpreal(1.0)/mpreal(2.0), mpreal(1.0)/mpreal(2.0), mpreal(1.0)/mpreal(2.0));
+    Vec3D actual = tetrahedron_center(r1, r2, r3, r4);
+
+#ifdef TEST_DEBUG_MESSAGES
+    std::cout.precision(50);
+    std::cout << "+---------------------------------------------------------------------------+" << std::endl;
+    std::cout << "|                  tetrahedron center (multiprecision)                      |" << std::endl;
+    std::cout << "+---------------------------------------------------------------------------+" << std::endl;
+    std::cout << "| variable        | value                                                   |" << std::endl;
+    std::cout << "+-----------------+---------------------------------------------------------+" << std::endl;
+    std::cout << "| expected x      | " << expected.x()                << string( 4, ' ') << "|" << std::endl;
+    std::cout << "| actual x        | " << actual.x()                  << string( 4, ' ') << "|" << std::endl;
+    std::cout << "| expected y      | " << expected.y()                << string( 4, ' ') << "|" << std::endl;
+    std::cout << "| actual y        | " << actual.y()                  << string( 4, ' ') << "|" << std::endl;
+    std::cout << "| expected z      | " << expected.z()                << string( 4, ' ') << "|" << std::endl;
+    std::cout << "| actual z        | " << actual.z()                  << string( 4, ' ') << "|" << std::endl;
+    std::cout << "+-----------------+---------------------------------------------------------+" << std::endl;
+#endif // TEST_DEBUG_MESSAGES
+
+    REQUIRE( abs(expected.x() - actual.x()) < eps );
+    REQUIRE( abs(expected.y() - actual.y()) < eps );
+    REQUIRE( abs(expected.z() - actual.z()) < eps );
+
+}
